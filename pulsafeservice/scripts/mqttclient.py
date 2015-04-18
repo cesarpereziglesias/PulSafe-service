3828fdfdd7ce44271aa837a7aec0ab3da95dceda
ouvigna <cesarpereziglesias@gmail.com>
2015-04-18 10:09:45 +0200

    Include mqtt scripts

--- pulsafeservice/scripts/mqttclient.py
+++ pulsafeservice/scripts/mqttclient.py
@@ -1,10 +1,14 @@
 import sys
+import json
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import MQTTv311
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.publish("pulsafe/thetest", "This is the content", qos=2)
+    message = {"position": "(42.42324, 8.12393)",
+               "bpm": 60.6}
+    #client.publish("pulsafe/users/cesar/HEARTRATE", json.dumps(message), qos=2)
+    client.publish("pulsafe/command/publish", json.dumps(message), qos=2)
 
 def on_disconnect(client, userdata, rc):
     print("Disconnected with result code "+str(rc))
@@ -19,10 +23,12 @@ def main(argv):
     client.on_disconnect = on_disconnect
     client.on_publish = on_publish
 
-    client.username_pw_set("somkzabk", "_phIeEBlWnxP")
-    client.connect("m20.cloudmqtt.com", 11741, 60)
+    client.connect("broker.mqttdashboard.com", 1883, 60)
 
-    client.loop_forever()
+    client.loop_start()
+    raw_input("Press a key to quit\n")
+    client.loop_stop()
+
 
 if __name__ == '__main__':
     main(sys.argv)
--- pulsafeservice/scripts/mqttservice.py
+++ pulsafeservice/scripts/mqttservice.py
@@ -1,35 +1,61 @@
 import sys
+import transaction
+
+from datetime import datetime
+
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import MQTTv311, MQTTv31
 
-from pyramid.paster import bootstrap
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import get_appsettings
+from pyramid.scripts.common import parse_vars
 
 from pulsafeservice.models import initialize_sql, DBSession, Log
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("pulsafe/+", 2)
+    client.subscribe("pulsafe/#", 2)
+
 
 def on_disconnect(client, userdata, rc):
     print("Disconnected with result code "+str(rc))
 
+
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    levels = msg.topic.split("/")
+    resource = levels[1]
+    if resource == "users":
+        save_user_log(levels[2], levels[3], msg.payload)
+    
+
+def save_user_log(user, type_log, value):
+    with transaction.manager:
+        l = Log(user=user,
+                datetime=datetime.now(),
+                type_log=type_log,
+                value=value)
+        DBSession.add(l)
 
 
 def main(argv):
+
+    config_uri = argv[1]
+    options = parse_vars(argv[2:])
+    settings = get_appsettings(config_uri, options=options)
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    initialize_sql(engine)
 
     client = mqtt.Client(protocol=3)
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = on_message
 
-    #client.username_pw_set("somkzabk", "_phIeEBlWnxP")
-    #client.connect("m20.cloudmqtt.com", 11741, 60)
-    #client.connect("test.mosquitto.org", 8884, 60)
     client.connect("broker.mqttdashboard.com", 1883, 60)
 
-    client.loop_forever()
+    client.loop_start()
+    raw_input("Press a key to quit\n")
+    client.loop_stop()
 
 if __name__ == '__main__':
     main(sys.argv)
